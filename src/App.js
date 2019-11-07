0becf227445877e2ca02920cd80a342cf0f8da2b
Anthony Agbator <tonjoe99@gmail.com>
2019-11-07 23:28:30 +0100

    updated the shop and the wordpress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+
+function About() {
+  return (
+    <h1>About Page is here</h1>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Homepage></Homepage>
-      </header>
+    <div>
+      <Switch>
+          <Route exact={true} path='/' component={Homepage} />
+          <Route exact={true} path='/shop' component={ShopPage} />
+          <Route path='/about' component={About} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,14 +12,14 @@ class Directory extends React.Component {
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
                     size: 'medium',
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
                     size: 'medium',
-                    linkUrl: 'shop/jackets'
+                    linkUrl: 'jackets'
                 },
                 {
                     title: 'sneakers',
@@ -33,14 +33,14 @@ class Directory extends React.Component {
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
-                    linkUrl: 'shop/womens'
+                    linkUrl: ''
                 },
                 {
                     title: 'mens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
-                    linkUrl: 'shop/mens'
+                    linkUrl: ''
                 }
             ]
         }
@@ -50,8 +50,8 @@ class Directory extends React.Component {
         return(
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, size, id}) =>
-                        <MenuItem key={id} size={size} imageUrl={imageUrl} title={title}></MenuItem>
+                    this.state.sections.map(({id, ...otherProps}) =>
+                        <MenuItem key={id} {...otherProps}></MenuItem>
                     )
                 }
             </div>
